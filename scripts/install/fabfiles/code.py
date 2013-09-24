4c70eec36650aff1c141c155c7d630bcfd7b7a44
Salvador Perez <sperez@conwet.com>
2013-09-24 12:28:30 +0200

    Add basic version of the installer/runner

---
+++ scripts/install/fabfiles/code.py
@@ -0,0 +1,66 @@
+from fabric.api import run,local
+from fabric.operations import put
+
+
+execute = run
+
+def install_base():
+
+    put('../base-dependencies.sh', '.')
+
+    execute('chmod +x /tmp/base-dependencies.sh; /tmp/base-dependencies.sh; rm ~/base-dependencies.sh')
+
+def clone():
+    put('../config/cli.env', '.')
+
+    execute('''
+        source ~/cli.env
+        mkdir "$REPO_NAME"
+        cd $REPO_NAME
+        virtualenv -p $PYTHON_PATH venv --distribute
+        source venv/bin/activate
+        git clone $REPO_URL "$REPO_NAME"
+        cd "$REPO_NAME"
+        git checkout $BRANCH
+        ''')
+
+def update():
+    execute('''
+        source ~/cli.env
+        cd "$REPO_NAME"
+        source venv/bin/activate
+
+        cd "$REPO_NAME"
+        git pull origin $BRANCH
+        sh ./scripts/install/project-dependencies.sh
+        pip install -r requirements.txt
+        wget --output-document .env $AWS_URL
+        python manage.py collectstatic --noinput
+        ''')
+
+def run():
+    execute('''
+    source ~/cli.env
+    cd "$REPO_NAME"
+    source venv/bin/activate
+    cd "$REPO_NAME"
+
+    wget --output-document .env $AWS_URL
+
+    nohup foreman start -e .env &>out.log &
+    exit
+    ''', pty=False)
+
+def logs():
+    execute('''
+    source ~/cli.env
+    cd "$REPO_NAME"/"$REPO_NAME"
+    tail -f out.log
+    ''')
+
+def stop():
+    execute('''
+        killall -9 foreman
+        killall -9 gunicorn
+        killall -9 python
+        ''')
