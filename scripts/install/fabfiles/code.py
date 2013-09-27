3e4a46d33e4b165ef932e96e9510a26886dd90a1
Salvador Perez <sperez@conwet.com>
2013-09-27 09:36:11 +0200

    Add vm creation to fabfiles operations

--- scripts/install/fabfiles/code.py
+++ scripts/install/fabfiles/code.py
@@ -1,8 +1,19 @@
+import os
 from fabric.api import run,local
 from fabric.operations import put
-
+from utils.awsconnector import launchInstances
 
 execute = run
+
+def vm():
+    config = open("config/cli.env",'r')
+    env = readlines(config)
+    config.close()
+    for var in env:
+        keyVal = split(var,"=")
+        os.environ[keyVal[0]] = keyVal[1]
+
+    launchInstances(os.environ[AWS_KEY],os.environ[INSTANCE_TYPE],os.environ[SECURITY_GROUP])
 
 def install_base():
 
