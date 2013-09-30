5a8bac766b0d6e5edd3d113c7ae682f57bd1d892
garinoth <cdavidrp@gmail.com>
2013-09-30 13:52:31 +0200

    Add support for editing .env file remotely

--- scripts/install/fabfiles/db.py
+++ scripts/install/fabfiles/db.py
@@ -4,6 +4,9 @@ from fabric.api import run, local
 
 execute = run
 
+import os
+from util.awsconnector import launchInstances, download, upload
+
 def vm():
     config = open("config/cli.env",'r')
     env = readlines(config)
@@ -12,12 +15,17 @@ def vm():
         keyVal = split(var,"=")
         os.environ[keyVal[0]] = keyVal[1]
     instance = launchInstances(os.environ[AWS_KEY],os.environ[INSTANCE_TYPE],os.environ[SECURITY_GROUP])
+    
     download('.env', '.env')
-    f = open('.env', 'w')
-    for line in f:
-        if 'DB_HOST' in line:
-            line=line.replace(line,'DB_HOST=' + instance.public_dns_name)
-    f.close()
+    with open('.env', 'r') as f:
+        lines = f.readlines()
+    
+    for i in range(len(lines)):
+        if 'DB_HOST' in lines[i]:
+            lines[i] = 'DB_HOST=' + instance.public_dns_name + '\n'
+    
+    with open('.env', 'w') as f:
+        f.writelines(lines)
     upload('.env','.env')
     os.remove('.env')
 
