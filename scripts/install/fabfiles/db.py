e9a31d8420b0d1dfa16174792a068e1b719acaa7
Salvador Perez <sperez@conwet.com>
2013-09-30 12:34:51 +0200

    Add .env managing when creating a database virtual machine

--- scripts/install/fabfiles/db.py
+++ scripts/install/fabfiles/db.py
@@ -1,12 +1,20 @@
 import os
-from utils.awsconnector import launchInstances
+from utils.awsconnector import launchInstances, download, upload
 
 def vm():
-	config = open("config/cli.env",'r')
+    config = open("config/cli.env",'r')
     env = readlines(config)
     config.close()
     for var in env:
         keyVal = split(var,"=")
         os.environ[keyVal[0]] = keyVal[1]
     instance = launchInstances(os.environ[AWS_KEY],os.environ[INSTANCE_TYPE],os.environ[SECURITY_GROUP])
-    
+    download('.env', '.env')
+    f = open('.env', 'w')
+    for line in f:
+        if 'DB_HOST' in line:
+            line=line.replace(line,'DB_HOST=' + instance.public_dns_name)
+    f.close()
+    upload('.env','.env')
+    os.remove('.env')
+
