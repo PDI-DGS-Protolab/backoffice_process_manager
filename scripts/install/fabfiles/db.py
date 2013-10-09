2b42209e03f40075ae7dcb89d7826f0f64003bae
Salvador Perez <sperez@conwet.com>
2013-10-09 11:34:08 +0200

    Add DB init functions

--- scripts/install/fabfiles/db.py
+++ scripts/install/fabfiles/db.py
@@ -1,21 +1,19 @@
 import os
-from util.awsconnector import launchInstances, download, upload
-from util.config_manager import get_local, set_local
+from utils.awsconnector import launchInstances, download, upload
 from fabric.api import run, local
 
 execute = run
 
+import os
+from util.awsconnector import launchInstances, download, upload
 
 CONFIG_FILE = 'config/db.env'
 
 
 def vm():
-    ami_id = get_local(CONFIG_FILE, 'AWS_AMI_ID')
-    type = get_local(CONFIG_FILE, 'AWS_INSTANCE_TYPE')
-    sec = get_local(CONFIG_FILE, 'AWS_SECURITY_GROUP')
-
+    type = get_local(CONFIG_FILE, 'INSTANCE_TYPE')
     instance = launchInstances(
-        os.environ['AWS_KEY_PAIR'], type, sec, ami_id)
+        os.environ['AWS_KEY_PAIR'], type)
 
     download('.env', '.env')
     set_local('.env', 'DB_HOST', instance.public_dns_name)
@@ -24,14 +22,6 @@ def vm():
 
     set_local(CONFIG_FILE, 'DNS', instance.public_dns_name)
     set_local(CONFIG_FILE, 'AWS_INSTANCE_ID', instance.id)
-
-
-def install():
-    execute('''
-        sudo yum -y install mysql mysql-server
-        sudo /sbin/chkconfig mysqld on
-        sudo /sbin/service mysqld start
-        ''')
 
 
 def sync():
@@ -44,14 +34,33 @@ def sync():
         ''')
 
 
+def init(dbName, username, password, host="*"):
+
+    createDb(dbName)
+    createUser(username, password, dbName)
+    authorize(username, host, dbName)
+
+
+def createDb(dbName):
+    execute('mysql -u root -p -e "CREATE DATABASE ' + dbName + '"')
+
+
+def createUser(username, password, dbName):
+    execute('mysql -u root -p -e "CREATE USER \'' + username + '\'@\'localhost\' IDENTIFIED BY \'' + password + \';"')
+
+
+def authorize(username, hostname, dbName=None):
+    if dbName:
+        execute('mysql -u root -p -e "GRANT ALL PRIVILEGES ON ' + dbName +'.* TO \'' + username + '\'@\'' + hostname + '\';"')
+    else:
+        execute('mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO \'' + username + '\'@\'' + hostname + '\';"')
+
 def help():
-    print '''
-    ROLE:
+    print '''ROLE:
         db
 
     ACTIONS:
         help            Show this message
         vm              Creates a VM in AWS and updates the .env files
-        install         Install the base dependencies for the database
         sync            Synchronize the database
-    '''
+        '''
